b1fb31c716bad58ba0487800f7179f8cf8b2e03f
jpe0824 <jason.e24@gmail.com>
2024-01-16 12:20:42 -0700

    theme and other setup

---
+++ frontend/src/theme.tsx
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#fff200",
+    },
+    secondary: {
+      main: "#7b00c8",
+    },
+  },
+});
+
+export default theme;

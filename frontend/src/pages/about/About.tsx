4ea7c66b4c64b2dbb0a4c5b1a9f41483130ec190
jpe0824 <jason.e24@gmail.com>
2024-01-19 12:02:00 -0700

    Finished about page

--- frontend/src/pages/about/About.tsx
+++ frontend/src/pages/about/About.tsx
@@ -5,10 +5,23 @@ import {
 } from "../../components/buttons/Buttons";
 import { Box, Grid, Paper } from "@mui/material";
 import styles from "../../styles/styles";
+import { useState } from "react";
 
 function About() {
+  const [isHovered1, setIsHovered1] = useState<boolean>(false);
+  const [isHovered2, setIsHovered2] = useState<boolean>(false);
+  const [isHovered3, setIsHovered3] = useState<boolean>(false);
+  const [isHovered4, setIsHovered4] = useState<boolean>(false);
+
+  const titleStyle1 = isHovered1 ? { marginBottom: 0 } : {};
+  const titleStyle2 = isHovered2 ? { marginBottom: 0 } : {};
+  const titleStyle3 = isHovered3 ? { marginBottom: 0 } : {};
+  const titleStyle4 = isHovered4 ? { marginBottom: 0 } : {};
+
   return (
-    <div style={{ flexGrow: 1, height: "80%", width: "100vw" }}>
+    <div
+      style={{ flexGrow: 1, height: "90vh", width: "100vw", overflow: "auto" }}
+    >
       <UpArrowButton page="/" direction="up" />
       <h1 style={styles.title}>About</h1>
       <Box
@@ -24,46 +37,115 @@ function About() {
       >
         <Grid container spacing={2}>
           {/* Top container */}
-          <Grid item xs={12}>
+          <Grid
+            item
+            xs={12}
+            onMouseEnter={() => setIsHovered1(true)}
+            onMouseLeave={() => setIsHovered1(false)}
+          >
             <Paper style={styles.paper}>
-              <h2>Creative Developer and Life-Long Learner</h2>
-              <p>
-                I have always loved technology and creating things. When I was
-                younger it came in the form of building Legos, then video games
-                like Minecraft or some city builder. Now I enjoy the creativity
-                and challenges of building, maintaining, and improving software.
-              </p>
+              <h2 style={titleStyle1}>
+                Creative Developer and Life-Long Learner
+              </h2>
+              {isHovered1 && (
+                <p>
+                  I have always loved technology and creating things. When I was
+                  younger it came in the form of building Legos, then video
+                  games like Minecraft or some city builder. Now I enjoy the
+                  creativity and challenges of building, maintaining, and
+                  improving software.
+                </p>
+              )}
             </Paper>
           </Grid>
 
           {/* Bottom three containers */}
-          <Grid item xs={12} sm={4}>
-            <Paper style={styles.paper}>
-              <h3>As a Developer</h3>
-              <p>
-                I pride myself in putting the best code forward. I take time to
-                ensure my code is{" "}
-                <b>
-                  <i>readable</i>
-                </b>{" "}
-                and{" "}
-                <b>
-                  <i>maintainable</i>
-                </b>
-                . I am always willing to take feedback and use that to improve
-                my skills.
-              </p>
-              <p>
-                I am always excited to work on a project. I look to learn
-                something new and am happy to lead the way when needed.
-              </p>
+          <Grid
+            item
+            xs={12}
+            sm={4}
+            onMouseEnter={() => setIsHovered2(true)}
+            onMouseLeave={() => setIsHovered2(false)}
+          >
+            <Paper style={styles.bottomPaper}>
+              <h2 style={titleStyle2}>As a Developer</h2>
+              {isHovered2 && (
+                <>
+                  <p>
+                    I pride myself in putting the best code forward. I take time
+                    to ensure my code is{" "}
+                    <b>
+                      <i>readable</i>
+                    </b>{" "}
+                    and{" "}
+                    <b>
+                      <i>maintainable</i>
+                    </b>
+                    . I am always willing to take feedback and use that to
+                    improve my skills.
+                  </p>
+                  <p>
+                    I am always excited to work on a project. I look to learn
+                    something new and am happy to lead the way when needed.
+                  </p>
+                </>
+              )}
             </Paper>
           </Grid>
-          <Grid item xs={12} sm={4}>
-            <Paper style={styles.paper}>Bottom Center Container</Paper>
+          <Grid
+            item
+            xs={12}
+            sm={4}
+            onMouseEnter={() => setIsHovered3(true)}
+            onMouseLeave={() => setIsHovered3(false)}
+          >
+            <Paper style={styles.bottomPaper}>
+              <h2 style={titleStyle3}>As a Colleague</h2>
+              {isHovered3 && (
+                <>
+                  <p>
+                    Communication is key. With my background in sales I have
+                    learned to communicate with{" "}
+                    <b>
+                      <i>every</i>
+                    </b>{" "}
+                    type of person. I am always happy to voice my opinion or
+                    welcome others' as well.
+                  </p>
+                  <p>
+                    I am a team player and happy to help my peers when needed. I
+                    prefer to go into what I call code mode (blast music for a
+                    couple hours and vigorously type away) but when needed,
+                    jumping on a call to break down a complicated problem can be
+                    just as fun.
+                  </p>
+                </>
+              )}
+            </Paper>
           </Grid>
-          <Grid item xs={12} sm={4}>
-            <Paper style={styles.paper}>Bottom Right Container</Paper>
+          <Grid
+            item
+            xs={12}
+            sm={4}
+            onMouseEnter={() => setIsHovered4(true)}
+            onMouseLeave={() => setIsHovered4(false)}
+          >
+            <Paper style={styles.bottomPaper}>
+              <h2 style={titleStyle4}>As a Person</h2>
+              {isHovered4 && (
+                <>
+                  <p>
+                    Work life balance is very important to me. I gravitated to
+                    this career because of just that.
+                  </p>
+                  <p>
+                    I make sure to take time for my family and myself. I love
+                    spending time with my kids, whether we are hiking, playing
+                    games or cozied up to a movie.
+                  </p>
+                </>
+              )}
+            </Paper>
           </Grid>
         </Grid>
       </Box>
--- frontend/src/styles/styles.ts
+++ frontend/src/styles/styles.ts
@@ -1,12 +1,26 @@
 const styles = {
   paper: {
-    padding: 8, // You can adjust the padding as needed
+    display: "flex" as const,
+    flexDirection: "column" as const,
+    justifyContent: "center" as const,
+    paddingLeft: 25, // You can adjust the padding as needed
     textAlign: "left" as const, // Centers the text inside the Paper
+    minHeight: "160px" as const,
+  },
+  bottomPaper: {
+    display: "flex" as const,
+    flexDirection: "column" as const,
+    justifyContent: "center" as const,
+    paddingLeft: 25, // You can adjust the padding as needed
+    textAlign: "left" as const, // Centers the text inside the Paper
+    minHeight: "325px" as const,
+    maxHeight: "325px" as const,
+    overflow: "auto" as const,
   },
   title: {
     flexDirection: "column" as const, // Use camelCase for CSS property names
     marginLeft: "5vw", // Use camelCase for CSS property names
-    marginTop: "7vh"
+    marginTop: "7vh",
   },
 };
 
